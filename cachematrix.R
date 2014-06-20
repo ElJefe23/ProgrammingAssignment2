80158ed219092be558f947106c66e544277be6f2
Jeff Wolbach <jeffrey.wolbach@verizon.net>
2014-06-20 19:13:31 -0400

    AddedComments
    Added comments to the cachematrix.R file.  Still don't completely follow
    the logic of using a list.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Combination of functions to cache the inverse of a matrix, in order to circumvent
+## a potentially lengthy calculation.  If the inverse of a particular matrix is required 
+## mutliple times, only the first instance will require the explicit calculation.  Subsequent
+## requests for the inverse will be read from the cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions which return a reference matrix, an input 
+## matrix, a function to calculate the inverse of the input matrix, and the reference inverse
+## makeCacheMatrix takes a matrix as a passed parameter
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <-NULL
@@ -16,7 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a matrix as a passed parameter.  The inverse of the matrix is calculated
+## if it has not been previously calculated (checked by if() statement).  If the inverse
+## has been previously calculated, the inverse is read off the list/cache instead of being
+## explicitly calculated.  The check only works for repeated calls with the same list name,
+## not repeated calls of different lists (from makeCacheMatrix) that were created from the
+## same matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
